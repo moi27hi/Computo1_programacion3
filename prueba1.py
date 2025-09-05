772492f7934060f700ee099a88eda94584f73e8f
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-04 22:05:17 -0600

    creacion de una clase

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,23 @@
 # Esto es una prueba
+
+class operacionesNumericas:
+
+    def __init__(self,numero1,numero2):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+
+    def sumar(self):
+        
+        return self.numero1 + self.numero2
+    
+    def imprimir_resultado():
+        print(f"El resultado de la suma es {operacionesNumericas.sumar()}")
+
+
+num1 = operacionesNumericas(5,6)
+num1.imprimir_resultado()
+
+
+
+        
