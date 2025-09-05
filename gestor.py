f9b7ab3ac5038a4f8c34ca0296779b81903facf6
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-04 22:39:38 -0600

    creacion de la clase gestor

---
+++ gestor.py
@@ -0,0 +1,22 @@
+# gestor.py
+from viaje import Viaje
+
+class GestorViajes:
+    def __init__(self):
+        self.viajes = []
+
+    def agregar_viaje(self, ruta, costo, tiempo):
+        viaje = Viaje(ruta, costo, tiempo)
+        self.viajes.append(viaje)
+
+    def mostrar_viajes(self):
+        print("\n--- LISTA DE VIAJES ---")
+        for i, viaje in enumerate(self.viajes, start=1):
+            print(f"{i}. {viaje}")
+
+    def resumen(self):
+        total_costo = sum(v.costo for v in self.viajes)
+        total_tiempo = sum(v.tiempo for v in self.viajes)
+        print("\n--- RESUMEN SEMANAL ---")
+        print(f"Gasto total: ${total_costo:.2f}")
+        print(f"Tiempo total: {total_tiempo} minutos")
