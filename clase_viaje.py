7cb4051f74d325fbe6af479896cd56d7b05ac00b
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-04 22:31:58 -0600

    creacion de la clase viaje

---
+++ clase_viaje.py
@@ -0,0 +1,10 @@
+# viaje.py
+
+class Viaje:
+    def __init__(self, ruta, costo, tiempo):
+        self.ruta = ruta
+        self.costo = costo
+        self.tiempo = tiempo
+
+    def __str__(self):
+        return f"Ruta: {self.ruta} | Costo: ${self.costo:.2f} | Tiempo: {self.tiempo} min"
